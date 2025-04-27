67d9787cfe22eb906ae69f4cbfede40c52c09dd8
asaduzzaman-sarker <asaduzzaman.seo@gmail.com>
2025-04-27 19:53:31 +0600

    Add two file

---
+++ server.py
@@ -0,0 +1,25 @@
+# calculator.py
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error: Division by zero."
+    return x / y
+
+def main():
+    print("Simple Calculator")
+    print("Add:", add(5, 3))
+    print("Subtract:", subtract(5, 3))
+    print("Multiply:", multiply(5, 3))
+    print("Divide:", divide(5, 3))
+
+if __name__ == "__main__":
+    main()
